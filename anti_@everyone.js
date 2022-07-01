ff7723c47c9c8f826360d643f6a75f62391036cf
Nine <79080468+john0128@users.noreply.github.com>
2022-07-01 18:53:10 +0900

    Create anti_@everyone.js

---
+++ anti_@everyone.js
@@ -0,0 +1,9 @@
+const { Permissions, MessageEmbed } = require('discord.js')
+client.on('messageCreate' async (msg) => {
+    if (msg.author.permission.has(Permissions.FLAGS.ADMINISTRATOR)) return ;
+    msg.delete()
+    const embed = new MessageEmbed()
+      .setTitle("에블핑 감지")
+      .setColor(0xff0000)
+    msg.channel.send({ embeds: [embed]})
+})
